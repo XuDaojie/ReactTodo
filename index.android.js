dd34726479c5941722dbd39009ea44c96f352519
XuDaojie <xudaojieg@gmail.com>
2016-12-11 22:42:25 +0800

    init commit

---
+++ index.android.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+// import {NativeModules} from 'react-native';
+
+export default class ReactTodo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+
+      // <View>
+      //   <TouchableHighlight
+      //     onPress={function onPressIn() {
+      //       NativeModules.ToastAndroid.show("Test", NativeModules.ToastAndroid.SHORT);
+      //     }}>
+      //     <Text style={styles.welcome}>Hello</Text>
+      //   </TouchableHighlight>
+      // </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactTodo', () => ReactTodo);
